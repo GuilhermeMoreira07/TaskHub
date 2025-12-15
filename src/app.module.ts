d532fd55a821e2c2def330a5ba5e3af23b5a9cf3
Guilherme Moreira <148500372+GuilhermeMoreira07@users.noreply.github.com>
2025-12-15 15:51:20 -0300

    Add: CRUD nas API e Comentários + Organização
    crud nas API's + comentários e organização das pastas do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-//importação dos modulos
+// Importações necessárias para definir o módulo principal da aplicação
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from './modules/tasks/tasks.module'; //Importa o task criado no src
 
+// Decorator que define este arquivo como um módulo do NestJS
 @Module({
+  // Módulos que o AppModule utiliza
   imports: [TasksModule],
+
+  // Controllers ligados diretamente ao AppModule
   controllers: [AppController],
+
+  // Services disponíveis para injeção de dependência
   providers: [AppService],
 })
 export class AppModule {}
